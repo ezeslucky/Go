1413ab87cc6034ecb3e74feba05c4031a7baa6aa
Lucky <lucky123@gmail.com>
2025-08-01 03:28:36 +0530

    constants

--- 3_variables/variabl.go
+++ 3_variables/variabl.go
@@ -5,7 +5,33 @@ import "fmt"
 func main() {
 	var xyz string = "lucky"
 	fmt.Println(xyz)
+// or
+
+var name = "go"
+fmt.Println(name)
 
 	var ty = false
 	fmt.Println(ty)
+
+	var age int = 30
+	fmt.Println(age)
+
+
+	// short variable declaration
+	name = "lucky"
+	fmt.Println(name)
+
+	abc := "hello"
+	fmt.Println(abc)
+
+
+	// float variable
+	var pi float32 = 3.141
+	fmt.Println(pi)
+	// short variable declaration
+	pi = 3.14
+	fmt.Println(pi)
+	// or
+	po := 3.14
+	fmt.Println(po)
 }
---
+++ 4_Constants/constants.go
@@ -0,0 +1,6 @@
+package main
+import "fmt"
+func main() {
+	const xyz string = "lucky"
+	fmt.Println(xyz)
+}
