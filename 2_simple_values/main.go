76fd292bda815ddf22a55d4ca5fe16e89b09f4bd
Lucky <lucky123@gmail.com>
2025-08-01 03:07:05 +0530

    go practics

---
+++ 2_simple_values/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	//integer
+	fmt.Println(1)
+	//strings
+	fmt.Println("helllo go ")
+	// booleans
+
+	fmt.Println(true)
+//float
+fmt.Println(10.5)
+
+
+}
---
+++ 3_variables/variabl.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var xyz string = "lucky"
+	fmt.Println(xyz)
+
+	var ty = false
+	fmt.Println(ty)
+}
