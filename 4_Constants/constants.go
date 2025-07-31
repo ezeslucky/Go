44d436930985480bd2e86b0cfa8e0026e1711bf7
Lucky <lucky123@gmail.com>
2025-08-01 03:42:02 +0530

    constants

--- 4_Constants/constants.go
+++ 4_Constants/constants.go
@@ -3,4 +3,14 @@ import "fmt"
 func main() {
 	const xyz string = "lucky"
 	fmt.Println(xyz)
+
+	const age int = 30
+	fmt.Println(age)
+
+	const (
+		port = 3000
+		host = "localhost"
+	)
+	fmt.Println("Port:", port)
+	fmt.Println("Host:", host)
 }
